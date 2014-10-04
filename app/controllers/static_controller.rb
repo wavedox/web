c991a038c517e90fee348a343bd53e0b398b2a45
Wagner Camarao <wcamarao@circleup.com>
2014-10-03 18:45:19 -0700

    Setup temporary redirect

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   respond_to :json
 
+  def index
+    redirect_to 'http://dcuo.herokuapp.com' if Rails.env == 'production'
+  end
+
   def ping
     respond_with(Hash)
   end
