c3a1b93c0f8bc37610370a64a5b16aab3520a5f8
Wagner Camarao <wcamarao@circleup.com>
2014-10-12 18:18:10 -0700

    Move routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'static#index'
   get 'feat-completed-count' => 'feats#completed_count'
   get 'ping' => 'static#ping'
-  get '*all' => 'static#index'
 
   constraints subdomain: 'www' do
     get ':path', to: redirect(subdomain: nil, path: '/%{path}'), any: /.*/
   end
+
+  get '*all' => 'static#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
