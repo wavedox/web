ac035ad643280f52bd2477059dc5a228934daa33
Wagner Camarao <wcamarao@circleup.com>
2014-10-12 18:09:46 -0700

    Launch it

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,6 @@
 class StaticController < ApplicationController
   respond_to :json
 
-  def index
-    redirect_to 'http://dcuo.herokuapp.com' if Rails.env == 'production'
-  end
-
   def ping
     respond_with(Hash)
   end
