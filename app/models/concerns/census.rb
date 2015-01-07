3ffe0b72577a6eaa74e294636d7fceb21dfd40ef
Wagner Camarao <wcamarao@circleup.com>
2015-01-06 18:52:15 -0800

    Restore census.rb

---
+++ app/models/concerns/census.rb
@@ -0,0 +1,35 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
+class Census
+  BASE_URL = 'http://census.soe.com'
+  BASE_GET_PATH = '/s:bytecode/json/get/dcuo:v1'
+  BASE_COUNT_PATH = '/s:bytecode/json/count/dcuo:v1'
+
+  def get(path, verbose=true)
+    puts "Census.get: #{BASE_URL}#{BASE_GET_PATH}#{path}" if verbose
+    request = Net::HTTP::Get.new(BASE_GET_PATH + path)
+    response = http.request(request)
+    JSON.parse(response.body)
+  end
+
+  def count(path, verbose=true)
+    puts "Census.count: #{BASE_URL}#{BASE_COUNT_PATH}#{path}" if verbose
+    request = Net::HTTP::Get.new(BASE_COUNT_PATH + path)
+    response = http.request(request)
+    count = JSON.parse(response.body)['count']
+    count >= 0 ? count : nil
+  end
+
+  def encode(string)
+    URI::encode(string)
+  end
+
+  private
+
+  def http
+    uri = URI.parse(BASE_URL)
+    @http ||= Net::HTTP.new(uri.host, uri.port)
+  end
+end
